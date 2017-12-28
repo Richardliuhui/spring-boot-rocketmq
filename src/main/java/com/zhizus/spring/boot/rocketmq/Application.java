0fa2d9a622134c3ddf2b039f7ac8bb8a460f959f
dempezheng <dempezheng@kugou.net>
2017-12-28 17:59:45 +0800

    Initial commit

---
+++ src/main/java/com/zhizus/spring/boot/rocketmq/Application.java
@@ -0,0 +1,15 @@
+package com.zhizus.spring.boot.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dempezheng on 2017/12/28.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/zhizus/spring/boot/rocketmq/annotation/RocketMQMessageListener.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.zhizus.spring.boot.rocketmq.annotation;
+
+import com.zhizus.spring.boot.rocketmq.enums.ConsumeMode;
+import com.zhizus.spring.boot.rocketmq.enums.SelectorType;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
+
+import java.lang.annotation.*;
+
+/**
+ * RocketMQMessageListener Created by aqlu on 2017/9/27.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface RocketMQMessageListener {
+
+    String consumerGroup();
+
+    String topic();
+
+    SelectorType selectorType() default SelectorType.TAG;
+
+    String selectorExpress() default "*";
+
+    ConsumeMode consumeMode() default ConsumeMode.CONCURRENTLY;
+
+    MessageModel messageModel() default MessageModel.CLUSTERING;
+
+    int consumeThreadMax() default 64;
+
+}
